003d7c4a0499104882b062c03552fc5ad71193d1
atsushi <green515tea@gmail.com>
2025-02-11 17:19:42 +0900

    draw learning curve

--- src/schedule_free_experiment/gnn/draw_loss.py
+++ src/schedule_free_experiment/gnn/draw_loss.py
@@ -24,7 +24,14 @@ BLUE = "#1E90FF"
 RED = "#F26649"
 
 LINE_COLOR = {"scheduler": BLUE, "scheduleFree": RED}
-LINE_STYLE = {"Train": "solid", "Val": "dashed"}
+LINE_STYLE = {
+    "Train": "solid",
+    "Val": "dashed",
+    "scheduler": "solid",
+    "scheduleFree": "dashed",
+}
+# 図のy軸の最大値（誤差の大きさをどこまで見るか）
+YMAX_DICT = {"Pubmed": 2, "Planetoid": 2, "Cora": 2.5}
 
 
 def plot_loss_comparison(base_dir: Path) -> None:
@@ -49,6 +56,70 @@ def plot_loss_comparison(base_dir: Path) -> None:
     datasetnames = list(sorted(datasetname_set))
     learning_rates = list(sorted(lr_set))
 
+    n_colors = len(learning_rates)
+    cmap = plt.get_cmap("jet", n_colors)
+    color_list = [cmap(i) for i in range(n_colors)]
+
+    # 全部描画
+    for datasetname in datasetnames:
+        ymax = YMAX_DICT[datasetname]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        fig_acc, ax_acc = plt.subplots(figsize=(10, 5))
+        for condition in conditions:
+            for i, lr in enumerate(learning_rates):
+                file_path = (
+                    base_dir
+                    / f"{condition}_loss_history_{datasetname}_init_lr={lr}.csv"
+                )
+                if os.path.exists(file_path):
+                    df = pd.read_csv(file_path)
+
+                    ax.plot(
+                        df["Epoch"],
+                        df["Test Loss"],
+                        label=f"{condition}_{lr}_Test",
+                        # color=LINE_COLOR[condition],
+                        color=color_list[i],
+                        linestyle=LINE_STYLE[condition],
+                        linewidth=0.8,
+                        # alpha=alpha_list[i],
+                        # linewidth=width_list[i],
+                    )
+                    ax_acc.plot(
+                        df["Epoch"],
+                        df["Test Acc"],
+                        label=f"{condition}_{lr}_Test",
+                        # color=LINE_COLOR[condition],
+                        color=color_list[i],
+                        linestyle=LINE_STYLE[condition],
+                        linewidth=0.8,
+                        # alpha=alpha_list[i],
+                        # linewidth=width_list[i],
+                    )
+        # 命名を誤ったのでここで修正
+        if datasetname == "Planetoid":
+            datasetname = "CiteSeer"
+        ax.set_title(f"Loss Comparison for {datasetname} Dataset")
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("Test Loss")
+        ax.legend(bbox_to_anchor=(1.05, 0.5), borderaxespad=0, loc="center left")
+        ax.set_xlim(0, 100)
+        ax.set_ylim(0, ymax)
+        fig.tight_layout()
+        fig.savefig(base_dir / f"{datasetname}_loss_comparison.png")
+        plt.close(fig)
+
+        ax_acc.set_title(f"Accuracy Comparison for {datasetname} Dataset")
+        ax_acc.set_xlabel("Epoch")
+        ax_acc.set_ylabel("Test Accuracy")
+        ax_acc.legend(bbox_to_anchor=(1.05, 0.5), borderaxespad=0, loc="center left")
+        ax_acc.set_xlim(0, 100)
+        ax_acc.set_ylim(0, 1)
+        fig_acc.tight_layout()
+        fig_acc.savefig(base_dir / f"{datasetname}_accuracy_comparison.png")
+        plt.close(fig_acc)
+
+    # conditionごとに比較する作図
     for condition in conditions:
         for datasetname in datasetnames:
             fig, ax = plt.subplots()
@@ -59,15 +130,11 @@ def plot_loss_comparison(base_dir: Path) -> None:
                 )
                 if os.path.exists(file_path):
                     df = pd.read_csv(file_path)
-                    # ax.plot(
-                    #     df["Epoch"],
-                    #     df["Train Loss"],
-                    #     label=f"{condition}_{datasetname}_{lr}_Train",
-                    # )
+
                     ax.plot(
                         df["Epoch"],
-                        df["Val Loss"],
-                        label=f"{condition}_{datasetname}_{lr}_Val",
+                        df["Test Loss"],
+                        label=f"{condition}_{datasetname}_{lr}_Test",
                     )
 
             plt.title(f"Loss Comparison for {condition}")
